2384c4a2d82cf90ebc98d9a454abbf2a1f13ce44
root <cjds@live.com>
2020-04-09 05:00:46 +0000

    made it work with 2 configurations

--- helpers.bash
+++ helpers.bash
@@ -1,9 +1,17 @@
 bb() {
-    CC=clang++-10 CXX=clang++-10 bazel build $@
+    bazel build --config=pi --spawn_strategy=local //...
 }
 
 bt() {
-    CC=clang++-10 CXX=clang++-10 bazel test $@
+    bazel test --config=pi --spawn_strategy=local //...
+}
+
+bbx86() {
+    bazel build //... --config x86
+}
+
+btx86() {
+    bazel test //... --config x86
 }
 
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
